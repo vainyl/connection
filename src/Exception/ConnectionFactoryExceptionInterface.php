85b0b36884e96aee200a357242e3865b7e43d59f
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-06-12 17:25:48 +0300

    Added ConnectionFactoryException

--- src/Exception/ConnectionFactoryExceptionInterface.php
+++ src/Exception/ConnectionFactoryExceptionInterface.php
@@ -13,13 +13,14 @@ declare(strict_types=1);
 namespace Vainyl\Connection\Exception;
 
 use Vainyl\Connection\Factory\ConnectionFactoryInterface;
+use Vainyl\Core\ArrayInterface;
 
 /**
  * Interface ConnectionFactoryExceptionInterface
  *
  * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
  */
-interface ConnectionFactoryExceptionInterface
+interface ConnectionFactoryExceptionInterface extends ArrayInterface, \Throwable
 {
     /**
      * @return ConnectionFactoryInterface
